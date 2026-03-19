1fab9234623bb5f31247d16cb7b77c8638a87aeb
Henrique Pacheco Alves <henriquepachecoalves7@gmail.com>
2026-03-18 21:33:34 -0300

    finish mock

--- App.tsx
+++ App.tsx
@@ -17,6 +17,16 @@ export default function App() {
   if (!fontsLoaded) return <ActivityIndicator />;
   return (
     <View className="flex-1 justify-end items-center bg-[#303644]">
+      <View className="mb-6 px-6 self-stretch">
+        <Text className="text-[#979AA2] text-2xl font-light text-right">
+          5.000 + 12.000
+        </Text>
+      </View>
+      <View className="mb-6 px-6 self-stretch">
+        <Text className="text-white font-light text-7xl text-right">
+          17.000
+        </Text>
+      </View>
       <View className="flex-row gap-2">
         <Button body="C" />
         <Button body="±" />
@@ -41,7 +51,7 @@ export default function App() {
         <Button body="3" type="grayButton" />
         <Button body="+" type="blueButton" />
       </View>
-      <View className="flex-row gap-2 mt-2 mb-6">
+      <View className="flex-row gap-2 mt-2 mb-10">
         <Button body="0" type="grayButton" width="w-[170px]" />
         <Button body="." type="grayButton" />
         <Button body="=" type="blueButton" />
